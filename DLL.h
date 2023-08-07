51e9e0fe5a4854d2bbfa80d850f8773450fa2972
RealFaceCode <53064610+RealFaceCode@users.noreply.github.com>
2023-08-07 09:04:45 +0200

    Update DLL.h

--- DLL.h
+++ DLL.h
@@ -6,7 +6,7 @@
 #ifdef _WIN32
     #include <windows.h>
     typedef HINSTANCE DLL_Type;
-    #define DLL_Load(path) LoadLibrary(path)
+    #define DLL_Load(path) LoadLibraryW(path)
     #define DLL_LoadFunc(dll, funcName) GetProcAddress(dll, funcName)
     #define DLL_Close(dll) FreeLibrary(dll)
 #elif __unix__
@@ -34,15 +34,41 @@ typedef DLL_Type DLL;
   #define API extern "C"
 #endif
 
-#define MakeFuncDefine(r_type, func_def, func_name, ...)    typedef r_type (*func_def)(__VA_ARGS__);\
-                                                            func_def func_name = nullptr;
-#define LoadFunc(dll, func_def, func_name) func_name = reinterpret_cast<func_def>(DLL_LoadFunc(dll, #func_name))
+#ifdef _WIN32
+  #ifdef _WIN64
+    #define CALL __cdecl 
+  #else
+    #define CALL __stdcall
+  #endif
+#else
+  #define CALL
+#endif
 
-DLL LoadDLL(const std::filesystem::path& path)
-{
-    DLL dll = DLL_Load(path.string().c_str());
+#define MakeFuncDefine(r_type, func_def, func_name, ...)      typedef CALL r_type (*func_def)(__VA_ARGS__);\
+func_def func_name = nullptr;
+#define LoadFunc(dll, func_def, func_name, func_name_string)  func_name = reinterpret_cast<func_def>(DLL_LoadFunc(dll, func_name_string))
+
+#ifdef _WIN32
+  DLL LoadDLL(const wchar_t* path)
+  {
+    DLL dll = DLL_Load(path);
     if(!dll)
-        std::cerr << "Failed to load DLL[" << path.filename() << "]" << std::endl;
+      std::wcout << "Failed to load library[" << path  << "]" << std::endl;
+      return dll;
+  }
+#elif __unix__
+  DLL LoadDLL(const char* path)
+  {
+    DLL dll = DLL_Load(path);
+    if(!dll)
+      std::cout << "Failed to load library[" << utf8String << "]" << std::endl;
+    
     return dll;
-}
+  }
+#else 
+  DLL LoadDLL(const char* path)
+  {
+    return nullptr;
+  }
+#endif
 #endif //DYNAMICLOADING_DLL_CORE_H
